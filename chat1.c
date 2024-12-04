81601a5a4588b70ed083afc9e908ebd583f757cd
JanFKowal <kowalj@msoe.edu>
2024-12-04 11:18:55 -0600

    Added 3rd chat outline

--- chat1.c
+++ chat1.c
@@ -1,6 +1,6 @@
 /*
 Contributors: Jan Kowal, Ryan Morgan
 Last update: 2024/4/2024
-Desc: Chat program betweeen 2 programs (Chat1.c and Chat2.c). This is
+Desc: Chat program betweeen 3 programs (Chat1.c, Chat2.c, and Chat3.c). This is
 the initial program that needs to be ran first to properly connect.
 */
