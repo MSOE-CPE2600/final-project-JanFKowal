ae755b9b921f19bda654918a2082c3e91466b2c5
JanFKowal <kowalj@msoe.edu>
2024-12-11 11:05:15 -0600

    c1 and c2 chatting back and forth

--- chat1.c
+++ chat1.c
@@ -8,103 +8,94 @@ the initial program that needs to be ran first to properly connect.
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <sys/socket.h>
-#include <netdb.h>
-#include <netinet/ip.h>
-#include <netinet/in.h>
-#include <arpa/inet.h>
 #include <unistd.h>
+#include <arpa/inet.h>
+#include <pthread.h>
 
 // define some constants
-#define MAX 256
-#define SERVER_HOST "localhost"
-#define SERVER_IP "172.17.76.112" //"127.0.0.1" $hostname -I
-#define BUFLEN 256
+#define MAX 1024
 #define SERVER_PORT 1234
 
-char line1[BUFLEN];
-char line2[BUFLEN];
-struct sockaddr_in server_addr, client_addr1, client_addr2;
-int mysock, csock1, csock2, r, len1, len2, n1, n2;
+int mysock, csock1, r;
+struct sockaddr_in server_addr, client_addr1;
+socklen_t len1;
+char line1[MAX];
 
-int server_init();
-
-int main (void)
-{
-  char line[MAX];
-  server_init();
-  while (1) {
-    printf("server: accepting new connection ...\n");
-    len1 = sizeof(client_addr1);
-    // len2 = sizeof(client_addr2);
-    csock1 = accept(mysock, (struct sockaddr *)&client_addr1, &len1);
-    if (csock1 < 0)
-    {
-      printf("server accept error \n");
+void *read_from_client(void *arg) {
+    while (1) {
+        int n = recvfrom(csock1, line1, MAX, 0, NULL, NULL);
+        if (n <= 0) {
+            printf("server client died, server loops\n");
+            close(csock1);
+            pthread_exit(NULL);
+        }
+        printf("server read %s\n", line1);
+        if (!strcmp(line1, "quit")) {
+            close(csock1);
+            pthread_exit(NULL);
+        }
     }
-    printf("server accepted a client connection from \n");
-    printf("Client: IP= %s port=%d \n", inet_ntoa(client_addr1.sin_addr), ntohs(client_addr1.sin_port));
-    // csock2 = accept(mysock, (struct sockaddr *)&client_addr2, &len2);
-    // if (csock2 < 0)
-    // {
-    //   printf("server accept error \n");
-    // }
-    // printf("server accepted a client connection from \n");
-    // printf("Client: IP= %s port=%d \n", inet_ntoa(client_addr2.sin_addr), ntohs(client_addr2.sin_port));
- 
-  while(1) {
-    n1 = recvfrom(csock1, line1, MAX, 0, NULL, NULL);
-    if (n1 == 0)
-    {
-      printf("server client died, server loops\n");
-      close(csock1);
-      break;
-    }
-    printf("server read %s\n", line1);
-    n1 = sendto(csock2, line1, MAX, 0, (struct sockaddr *)&client_addr2, sizeof(client_addr2));
-    n1 = write(csock2, line1, MAX);
-    printf("server wrote %s\n", line1);
-    if (!strcmp(line1,"quit")) {
-      return 0;
-    }
-    // n2 = recvfrom(csock2, line2, MAX, 0, NULL, NULL);
-    // if (n2 == 0)
-    // {
-    //   printf("server client died, server loops\n");
-    //   close(csock2);
-    //   break;
-    // }
-    // printf("server read %s\n", line2);
-    // n2 = sendto(csock1, line2, MAX, 0, (struct sockaddr *)&client_addr1, sizeof(client_addr1));
-    // n2 = write(csock1, line2, MAX);
-    // printf("server wrote %s\n", line2);
-    // if (!strcmp(line2,"quit")) { 
-    //   return 0;
-    // }
-  }
-  }
-  return 0;
 }
 
-int server_init()
-{
-  printf("create stream socket\n");
-  mysock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
-  if (mysock < 0)
-  {
-    printf("socket call failed\n");
-  }
-  printf("fill server address with host IP and Port number\n");
-  server_addr.sin_family = AF_INET;
-  server_addr.sin_addr.s_addr = htonl(INADDR_ANY);
-  server_addr.sin_port = htons(SERVER_PORT);
-  printf("bind the socket");
-  r = bind(mysock, (struct sockaddr*)&server_addr, sizeof(server_addr));
-  if (r<0)
-  {
-    printf("bind failed\n");
-  }
-  printf("server is listening  ...\n");
-  listen(mysock, 5); // queue length of 5
-  printf("server init done\n");
+void *write_to_client(void *arg) {
+    while (1) {
+        fgets(line1, MAX, stdin);
+        line1[strlen(line1) - 1] = 0; // remove the \n
+        int n = sendto(csock1, line1, strlen(line1), 0, (struct sockaddr *)&client_addr1, sizeof(client_addr1));
+        printf("server wrote %s\n", line1);
+        if (!strcmp(line1, "quit")) {
+            close(csock1);
+            pthread_exit(NULL);
+        }
+    }
+}
+
+int server_init() {
+    printf("create stream socket\n");
+    mysock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+    if (mysock < 0) {
+        printf("socket call failed\n");
+        return -1;
+    }
+    printf("fill server address with host IP and Port number\n");
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+    server_addr.sin_port = htons(SERVER_PORT);
+    printf("bind the socket\n");
+    r = bind(mysock, (struct sockaddr*)&server_addr, sizeof(server_addr));
+    if (r < 0) {
+        printf("bind failed\n");
+        return -1;
+    }
+    printf("server is listening ...\n");
+    listen(mysock, 5);
+    printf("server init done\n");
+    return 0;
+}
+
+int main() {
+    if (server_init() < 0) {
+        return -1;
+    }
+
+    while (1) {
+        printf("server: accepting new connection ...\n");
+        len1 = sizeof(client_addr1);
+        csock1 = accept(mysock, (struct sockaddr *)&client_addr1, &len1);
+        if (csock1 < 0) {
+            printf("server accept error\n");
+        }
+        printf("server accepted a client connection from\n");
+        printf("Client: IP= %s port=%d\n", inet_ntoa(client_addr1.sin_addr), ntohs(client_addr1.sin_port));
+
+        pthread_t read_thread, write_thread;
+        pthread_create(&read_thread, NULL, read_from_client, NULL);
+        pthread_create(&write_thread, NULL, write_to_client, NULL);
+
+        pthread_join(read_thread, NULL);
+        pthread_join(write_thread, NULL);
+    }
+
+    close(mysock);
+    return 0;
 }
--- chat2.c
+++ chat2.c
@@ -1,9 +1,10 @@
 /*
 Contributors: Jan Kowal, Ryan Morgan
 Last update: 2024/4/2024
-Desc: Chat program betweeen 3 programs (Chat1.c, Chat2.c, Chat3.c). This is
-a secondary program to be ran after Chat1.c to properly connect.
+Desc: Chat program between 3 programs (Chat1.c, Chat2.c, and Chat3.c). This is
+a secondary program to be run after Chat1.c to properly connect.
 */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -13,58 +14,80 @@ a secondary program to be ran after Chat1.c to properly connect.
 #include <netinet/in.h>
 #include <arpa/inet.h>
 #include <unistd.h>
+#include <pthread.h>
+
 // define some constants
-#define MAX 256
-#define SERVER_HOST "localhost"
-#define SERVER_IP "172.17.76.112" //"127.0.0.1" $hostname -I
-#define BUFLEN 256
+#define MAX 1024
+#define SERVER_IP "172.18.5.105" // Replace with the actual server IP
 #define SERVER_PORT 1234
 
-char line[BUFLEN];
+char line[MAX];
 struct sockaddr_in server_addr;
 int sock, r;
 
-int client_init();
-
-int main (void)
-{
-  int n;
-  char line[MAX], ans[MAX];
-  client_init();
-  while (strcmp(line,"quit")) // processing loop
-  {
-    printf("input a line\n");
-    bzero(line, MAX);
-    fgets(line, MAX, stdin);
-    line[strlen(line)-1] = 0; //remove the \n
-    n = write(sock, line, MAX);
-    printf("client wrote %s\n", line);
-    n = read(sock, ans, MAX);
-    printf("client read %s\n", ans);
-  }
- 
-  return 0;
+void *read_from_server(void *arg) {
+    char ans[MAX];
+    while (1) {
+        int n = read(sock, ans, MAX);
+        if (n <= 0) {
+            printf("server disconnected\n");
+            close(sock);
+            pthread_exit(NULL);
+        }
+        printf("client read %s\n", ans);
+    }
 }
 
-int client_init()
-{
-  printf("create stream socket\n");
-  sock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
-  if (sock < 0)
-  {
-    printf("socket call failed\n");
-  }
-  printf("fill server address with host IP and Port number\n");
-  server_addr.sin_family = AF_INET;
-  server_addr.sin_addr.s_addr = htonl(INADDR_ANY);
-  server_addr.sin_port = htons(SERVER_PORT);
-  printf("connect to server");
-  r = connect(sock, (struct sockaddr*)&server_addr, sizeof(server_addr));
-  if (r<0)
-  {
-    printf("connect failed\n");
-  }
-  printf("connected ok  ...\n");
- 
-  printf("client init done\n");
+void *write_to_server(void *arg) {
+    while (1) {
+        printf("input a line\n");
+        bzero(line, MAX);
+        fgets(line, MAX, stdin);
+        line[strlen(line) - 1] = 0; // remove the \n
+        int n = write(sock, line, MAX);
+        printf("client wrote %s\n", line);
+        if (!strcmp(line, "quit")) {
+            close(sock);
+            pthread_exit(NULL);
+        }
+    }
+}
+
+int client_init() {
+    printf("create a TCP socket\n");
+    sock = socket(AF_INET, SOCK_STREAM, 0);
+    if (sock < 0) {
+        printf("socket call failed\n");
+        return -1;
+    }
+
+    printf("fill server_addr with server IP and PORT#\n");
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_addr.s_addr = inet_addr(SERVER_IP);
+    server_addr.sin_port = htons(SERVER_PORT);
+
+    printf("connecting to server ...\n");
+    r = connect(sock, (struct sockaddr *)&server_addr, sizeof(server_addr));
+    if (r < 0) {
+        perror("connect failed");
+        return -1;
+    }
+    printf("connected to server\n");
+    return 0;
+}
+
+int main(void) {
+    if (client_init() < 0) {
+        return -1;
+    }
+
+    pthread_t read_thread, write_thread;
+    pthread_create(&read_thread, NULL, read_from_server, NULL);
+    pthread_create(&write_thread, NULL, write_to_server, NULL);
+
+    pthread_join(read_thread, NULL);
+    pthread_join(write_thread, NULL);
+
+    close(sock);
+    return 0;
 }
